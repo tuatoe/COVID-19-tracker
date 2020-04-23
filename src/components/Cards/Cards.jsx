5e3c593db789be3a7fc1437ada82ffdfe2db2c0a
Tuatoe Daynuah <tuatoedaynuah@Tuatoes-iMac-2.hsd1.mn.comcast.net>
2020-04-23 18:08:39 -0500

    content update

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -29,7 +29,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                             <CountUp start={0} end={recovered.value} duration={2.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of recoveries cases of COVID-19</Typography>
+                        <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                     </CardContent>
                 </Grid>
 
@@ -40,7 +40,7 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                             <CountUp start={0} end={deaths.value} duration={2.5} separator="," />
                         </Typography>
                         <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-                        <Typography variant="body2">Number of deaths case by COVID-19</Typography>
+                        <Typography variant="body2">Number of deaths caused by COVID-19</Typography>
                     </CardContent>
                 </Grid>
             </Grid>
